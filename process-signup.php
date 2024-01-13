1cddb1f870afc6605b117df08e2c5f8a84bf1c45
jas <152701916+jastineedg@users.noreply.github.com>
2024-01-13 17:12:02 +0800

    Add files via upload

---
+++ process-signup.php
@@ -0,0 +1,154 @@
+<html>
+    <body>
+<?php
+
+if (empty($_POST["firstname"])) {
+    echo '<a href="signup.php">Go Back</a>';
+    die("Name is required");
+    
+}
+
+if ( ! filter_var($_POST["lastname"], FILTER_VALIDATE_EMAIL)) {
+    echo '<a href="signup.php">Go Back</a>';
+    die("Valid email is required");
+}
+if (empty($_POST["password"])) {
+    echo '<a href="signup.php">Go Back</a>';
+    die("Password is required");
+}
+if (strlen($_POST["password"]) < 8) {
+    echo '<a href="signup.php">Go Back</a>';
+    die("Password must be at least 8 characters");
+}
+
+if ( ! preg_match("/[a-z]/i", $_POST["password"])) {
+    echo '<a href="signup.php">Go Back</a>';
+    die("Password must contain at least one letter");
+}
+if ( ! preg_match("/[!@#$%^&*()_+.]/", $_POST["password"])) {
+    echo '<a href="signup.php">Go Back</a>';
+    die("Password must contain at least one special character");
+}
+if ( ! preg_match("/[0-9]/", $_POST["password"])) {
+    echo '<a href="signup.php">Go Back</a>';
+    die("Password must contain at least one number");
+}
+
+if ($_POST["password"] !== $_POST["password_confirmation"]) {
+    echo '<a href="signup.php">Go Back</a>';
+    die("Passwords must match");
+    
+}
+
+if (empty($_POST["degree"])) {
+    echo '<a href="signup.php">Go Back</a>';
+    die("Degree is required");
+    
+}
+if (empty($_POST["year"])) {
+    echo '<a href="signup.php">Go Back</a>';
+    die("Year is required");
+    
+}
+if (empty($_POST["studno"])) {
+    echo '<a href="signup.php">Go Back</a>';
+    die("Student Number is required");
+    
+}
+
+$password = password_hash($_POST["password"], PASSWORD_DEFAULT);
+
+$conn = require __DIR__ . "/database.php";
+
+        $firstname = $_POST['firstname'];
+		$lastname = $_POST['lastname'];
+		$password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+        $studno = $_POST['studno'];
+        $degree = $_POST['degree'];
+        $year = $_POST['year'];
+        $existingStudno = $_POST["studno"];
+        
+$checkQuery = "SELECT COUNT(*) as count FROM voters WHERE studno = ?";
+$stmtCheck = $conn->prepare($checkQuery);
+$stmtCheck->bind_param("s", $existingStudno);
+$stmtCheck->execute();
+$resultCheck = $stmtCheck->get_result();
+$rowCheck = $resultCheck->fetch_assoc();
+$count = $rowCheck['count'];
+
+if ($count > 0) {
+    echo '<a href="signup.php">Go Back</a>';
+    die("Student Number already exists");
+}
+//generate voters id
+$set = '123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+$voter = substr(str_shuffle($set), 0, 15);
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+//Load Composer's autoloader
+require 'PHPMailer/src/Exception.php';
+require 'PHPMailer/src/PHPMailer.php';
+require 'PHPMailer/src/SMTP.php';
+
+//Create an instance; passing `true` enables exceptions
+$mail = new PHPMailer(true);
+
+try {
+    //Server settings                 
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = 'plm.harivote@gmail.com';                     //SMTP username
+    $mail->Password   = 'pbzngrvmvpreadnv';                               //SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+    $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+    //Recipients
+    $mail->setFrom('from@example.com', 'PLM Harivote');
+    $mail->addAddress($_POST["lastname"]);   //Add a recipient
+    $mail->isHTML(true);
+
+    
+    $regmessage= "Greetings, ". $firstname . "! The code below is your assigned Voter's ID. Please utilize on vote tab when about to vote.\n\n\n" . $voter;
+
+    //Content
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject = 'PLM E-Voting';
+    $mail->Body    = $regmessage;
+
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
+
+
+$sql = "INSERT INTO voters (voters_id, password, firstname, lastname, studno, degree, year) VALUES ('$voter', '$password', '$firstname', '$lastname', '$studno', '$degree', '$year')";
+        
+$stmt = $conn->stmt_init();
+
+if ( ! $stmt->prepare($sql)) {
+    die("SQL error: " . $conn->error);
+}
+
+
+                  
+if ($stmt->execute()) {
+
+    header("Location: signup-success.php");
+    exit;
+    
+} else {
+    
+    if ($conn->errno === 1062) {
+        die("email already taken");
+    } else {
+        die($conn->error . " " . $conn->errno);
+    }
+}
+
+
+
---
+++ signup.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <!----======== CSS ======== -->
+    <link rel="stylesheet" href="signup.css">
+     
+    <!----===== Iconscout CSS ===== -->
+    <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
+
+   <title>HariVote Registration </title>
+</head>
+<body>
+<div id="page">
+        <header id="header">
+            <nav class="navbar">
+                <div id="logo">
+          <a href="loginuser.php"> <img height="80px" width="220px" src="img/logofinal.png"></a>
+        
+
+
+                </div>
+                <a href="#" class="toggle-button">
+                    <span class="bar"></span>
+                    <span class="bar"></span>
+                    <span class="bar"></span>
+                </a>
+                
+            </nav>
+        </header>
+    <main>
+    <div class="wrapper">
+        <div class="titles">
+            Create an account
+            <p>It's quick and easy</p>
+        </div>
+    <div class="container">
+        <header>HariVote Registration</header>
+
+        <form action="process-signup.php" method="post" id="signup" novalidate>
+            <div class="form first">
+                <div class="details personal">
+                    <span class="title">Personal Details</span>
+
+                    <div class="fields">
+                    
+                    <div class="input-field">
+                    <label for="firstname">Full Name</label>
+                      <input type="text" id="firstname" placeholder="Enter your Full Name" name="firstname"required>
+            
+                        </div>
+                        
+                        <div class="input-field">
+                            <label for="lastname">Personal Email</label>
+                            <input type="email" id="lastname" placeholder="Personal Email" name="lastname"required>
+                              </div>
+                        <div class="input-field">
+                        <label for="password">Password</label>
+                     <input type="password" id="password"placeholder="Enter your Password" name="password"required>
+                     
+            
+                        </div>
+        
+                         <div class="input-field">
+                         <label for="password_confirmation">Confirm password</label>
+                       <input type="password" id="password_confirmation" placeholder="Confirm Password" name="password_confirmation"required>
+                          </div>
+
+                          
+                          
+                    </div>
+                </div>
+
+                <div class="details ID">
+                    <span class="title">Identity Details</span>
+                    <div class="fields">
+                    
+                    <div class="input-field">
+                            <label label for="studno">Student Number</label>
+                            <input  id="studno" placeholder="Ex. 202100123" name="studno"required>
+                        </div>
+                    <div class="input-field">
+                      
+                            <label label for="degree">Degree or Program</label>
+                            <select id="degree" name="degree" required>
+                                <option disabled selected>Select Year</option>
+                                <option>Computer Engineering</option>
+                            </select>
+                        </div>
+                        <div class="input-field">
+                            <label label for="year">Year</label>
+                            <select id="year" name="year" required>
+                                <option disabled selected>Select Year</option>
+                                <option>1st</option>
+                                <option>2nd</option>
+                                <option>3rd</option>
+                                <option>4th</option>
+                                <option>5th</option>
+                                <option>6th</option>
+                            </select>
+                        </div>
+                       
+                    </div>
+
+                    <button class="nextBtn">
+                        <span class="btnText">Submit</span>
+                        <i class="uil uil-navigator"></i>
+                    </button>
+                </div> 
+            </div>
+
+        
+    <script src="signup.js"></script>
+</body>
+</html>
